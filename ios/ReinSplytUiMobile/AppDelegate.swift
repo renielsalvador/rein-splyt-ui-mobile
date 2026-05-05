d49d357b15a8e5d5378cfa77c32d8d60ce83ab62
rensalvador <reniel.salvador@gmail.com>
2026-05-05 19:17:31 +0800

    chore: update app name

--- ios/ReinSplytUiMobile/AppDelegate.swift
+++ ios/ReinSplytUiMobile/AppDelegate.swift
@@ -24,7 +24,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     window = UIWindow(frame: UIScreen.main.bounds)
 
     factory.startReactNative(
-      withModuleName: "ReinSplytUiMobile",
+      withModuleName: "Splyt",
       in: window,
       launchOptions: launchOptions
     )
